3f03dba6cf06a8db7e3968fc1faf92bfe65bb1e5
Roger Primo Jr <Roger Primo Jr>
2016-03-04 08:53:34 +0800

    firstdraft

---
+++ productscrapy/items.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ProductscrapyItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    description = scrapy.Field()
+    retailer_sku_code = scrapy.Field()
+    model = scrapy.Field()
+    mpn = scrapy.Field()
+    sku = scrapy.Field()
+    upc = scrapy.Field()
+    ean = scrapy.Field()
+    currency = scrapy.Field()
+    price = scrapy.Field()
+    crawl_time = scrapy.Field()
+    promo_price = scrapy.Field()
+    promo_qty = scrapy.Field()
+    promo_data = scrapy.Field()
+    promo_expiry = scrapy.Field()
+    current_price = scrapy.Field()
+    brand = scrapy.Field()
+    primary_image_url = scrapy.Field()
+    image_urls = scrapy.Field()
+    categories = scrapy.Field()
+    attributes = scrapy.Field()
+    features = scrapy.Field()
+    rating = scrapy.Field()
+    instock = scrapy.Field()
+    
---
+++ productscrapy/spiders/allforyou_sg_crawler.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class Allforyousgcrawler(scrapy.Spider):
+    name = "allforyou_sg_crawler"
+    allowed_domains = ["allforyou.sg"]
+    start_urls = (
+        'https://www.allforyou.sg/',
+    )
+
+    def parse(self, response):
+        for parse in response.xpath('//*[@id="pagemaincontent"]/div[1]/div[2]/div/div/div/div/div[2]/a/@href').extract():
+          print parse  
+
+        
