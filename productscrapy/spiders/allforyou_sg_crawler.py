56cffb17127ef718c215206433dc791a9109f874
Roger Primo Jr <Roger Primo Jr>
2016-03-04 15:04:58 +0800

    priceandnamedetails

--- productscrapy/spiders/allforyou_sg_crawler.py
+++ productscrapy/spiders/allforyou_sg_crawler.py
@@ -12,9 +12,15 @@ class Allforyousgcrawler(scrapy.Spider):
     def parse(self, response):
         for parse in response.xpath('//*[@id="pagemaincontent"]/div[1]/div[2]/div/div/div/div/div[2]/a/@href').extract():
             yield scrapy.Request(response.urljoin(parse), callback=self.parse_dir_contents)
-            #print response.urljoin(parse) 
+            # print response.urljoin(parse) 
             #printing of main page link
 
     def parse_dir_contents(self, response):
         for contents in response.xpath('//*[@id="pagemaincontent"]/div[1]/div[2]/div/div/h2/a/@href').extract():
-            print response.urljoin(contents)
+            # print response.urljoin(contents)
+            yield scrapy.Request(response.urljoin(contents), callback=self.details)
+
+    def details(self, response):
+        for details in response.xpath('//div[@class="prod-data"]/@id').extract():
+            print response.xpath('//div[@id="'+details+'"]//@data-name').extract()[0]
+            print response.xpath('//div[@id="'+details+'"]//@data-price').extract()[0]
